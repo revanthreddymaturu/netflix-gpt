8f0fb6bb864d85e2ff4df8144fb0d72957012fb8
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-12-10 15:04:12 -0500

    Slice Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Body from './components/Body';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 function App() {
   return (
-    <div className='w-[100%]'><Body/></div>
-   
+   <Provider store={appStore}>
+   <div className='w-[100%]'><Body/></div>
+   </Provider>
   );
 }
 
