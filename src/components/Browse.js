07c261e3e88c7f2182069a83ffcc11639a8910fb
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-12-21 11:21:54 -0500

    TMDB API Setup

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react"
 import Header from "./Header"
+import { TMDB_API_Options } from "../utils/constants"
 
 const Browse=()=>{
+    const getNowPlayingMovies=async ()=>{
+        const data=await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',TMDB_API_Options );
+        const json=await data.json();
+        console.log(json.results);
+    }
+    useEffect(()=>{
+        getNowPlayingMovies();
+    },[])
     return(
         <div>
         <Header/>
